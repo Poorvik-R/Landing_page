684a3a82cf847dfb88109dc02bb1858dec705075
Poorvik-R <rprao2004@gmail.com>
2026-04-20 23:26:37 +0530

    added connection to the static files

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI , Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get('/',response_class=HTMLResponse)
-
 def home(request: Request):
     return templates.TemplateResponse(
         name="index.html",
-        context={"request": request}
+        request = request,
     )
